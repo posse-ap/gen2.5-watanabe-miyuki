b42584e6ac95b6c86586fe541a6329f30aedbd74
watanabe-miyuki <watanabemiyuki@keio.jp>
2021-11-27 15:30:16 +0900

    見た目完成しました。

---
+++ quizy.js
@@ -0,0 +1,33 @@
+var correct = document.getElementById('correct');
+
+// console.log(correct);
+
+var correctbox = document.getElementById('correctbox');
+
+function correctaction(){
+  correct.style.backgroundColor = "blue"
+  correct.style.color = "white"
+  correctbox.style.display = "block"
+}
+
+
+
+var noncorrect1 = document.getElementById('noncorrect1');
+
+var noncorrectbox = document.getElementById('noncorrectbox');
+
+function noncorrectaction1(){
+  noncorrect1.style.backgroundColor = "red"
+  noncorrect1.style.color = "white"
+  noncorrectbox.style.display = "block"
+}
+
+
+var noncorrect2 = document.getElementById('noncorrect2');
+
+function noncorrectaction2(){
+  noncorrect2.style.backgroundColor = "red"
+  noncorrect2.style.color = "white"
+  noncorrectbox.style.display = "block"
+}
+
