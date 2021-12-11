8b61194d320b0122980b5e86b9738f0208dff1a4
watanabe-miyuki <watanabemiyuki@keio.jp>
2021-12-11 19:25:27 +0900

    for文うまくいかない

--- quizy.js
+++ quizy.js
@@ -10,22 +10,59 @@ let images = [
   "https://d1khcm40x1j0f.cloudfront.net/quiz/50a753d151d35f8602d2c3e2790ea6e4.png",//8
   "https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d812ea26e.png",//9
   "https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png"//10
-]
+];
 
+//  [不正解、正解、不正解]
+let options = [//options[i][0]
+  ["こうわ", "たかなわ", "たかわ"],
+  ["かめと", "かめいど","かめど"],
+  ["おかとまち", "こうじまち", "かゆまち"],
+  ["ごせいもん", "おなりもん", "おかどもん"],
+  ["たたら", "とどろき", "たたりき"],
+  ["せきこうい", "しゃくじい", "いじい"],
+  ["ざっしき", "ぞうしき", "さっしょく"],
+  ["ごしろちょう", "おかちまち", "みとちょう"],
+  ["ろっこつ", "ししぼね", "しこね"],
+  ["こばく", "こぐれ", "こしゃく"],
+  ];
 
+for (let j = 0; j < 10; j++) {
+  let quiz =
+  '<div class="question-inner">'
+  + '<h2 class="question-title">'
+  +  `<span class="under">${j+1}. この地名はな</span>んて読む？`
+  + '</h2>'
+  + `<img src="${images[j]}" alt="${options[j][1]}">`
+  + `<ul class="question-list" id ="question-list${j}">`
+  +  `<li class="question-list-item-nonCorrect" id = "item0">${options[j][0]}</li>`
+  +    `<li class="question-list-item-correct" id = "item1">${options[j][1]}</li>`
+  +  `<li class="question-list-item-nonCorrect" id = "item2">${options[j][2]}</li>`
+  +  '</div>'
+  + '<div class="question-correctBox">'
+  +  '<h3<span class="question-correctBox-title">正解！</span></h3>'
+  +  `<p class="question-correctBox-description">正解は「${options[j][1]}」です！</p>`
+  +'</div>'
+  + '<div class="question-nonCorrectBox">'
+  +  '<h3><span class="question-nonCorrectBox-title">不正解！</span></h3>'
+  +  `<p class="question-correctBox-description">正解は「${options[j][1]}」です！</p>`
+  + '</div>'
+  + '</div>';
+
+  document.getElementById('quizLocation').insertAdjacentHTML('beforebegin', quiz);
 
 //////////////////一つのクイズの動作完成//////////////////
 
 // js-nonCorrectBox 非表示
-const correctBox = document.getElementById("js-correctBox");
+const correctBox = document.querySelector('.question-correctBox');
 correctBox.style.display ="none";
-const nonCorrectBox = document.getElementById("js-nonCorrectBox");
+const nonCorrectBox = document.querySelector('.question-nonCorrectBox');
 nonCorrectBox.style.display ="none";
 const correctItem = document.querySelector('.question-list-item-correct');
+var questionList= document.getElementById(`question-list${j}`);
 
 // question-list-item-js-nonCorrectをクリック→
 // .question-list-item-js-nonCorrect(css)適応＆　js-nonCorrectBox非表示を解除＆クリックは一度
-document.querySelector('.question-list').addEventListener('click', e => {
+questionList.addEventListener('click', e => {
     if (e.target.className === 'question-list-item-nonCorrect') {
     e.target.classList.add('question-list-item-changing-color-nonCorrect');
     correctItem.classList.add('question-list-item-changing-color-correct');
@@ -57,8 +94,9 @@ for(i = a.length -1;i>0;i--){
 var itemShuffle0= document.getElementById(`item${a[0]}`);
 var itemShuffle1= document.getElementById(`item${a[1]}`);
 var itemShuffle2= document.getElementById(`item${a[2]}`);
-var questionList = document.getElementById('question-list');
 
 questionList.appendChild(itemShuffle0);
 questionList.appendChild(itemShuffle1);
 questionList.appendChild(itemShuffle2);
+
+};
