33e791e4daa6f336523363d41eec4a5e7aff858f
watanabe-miyuki <watanabemiyuki@keio.jp>
2022-03-22 15:09:55 +0900

    完成

--- .vscode/quizy.js
+++ .vscode/quizy.js
@@ -10,6 +10,33 @@ const quiz_lists = [
   ['こうじまち', 'おかとまち', 'かゆまち']
 ]
 
+// どうクラスをもってくるか考えている。
+function check(quiz_list_item, answer) {
+  const checked_list = document.getElementById(`quiz_${quiz_list_item}`);
+  const answer_list = document.getElementById(`quiz_${answer}`);
+  const answer_text = document.getElementById(`quiz_answer_${answer}`);
+
+  if (quiz_list_item === answer){
+  checked_list.classList.add("answer_valid");
+  answer_text.innerHTML = '正解';
+  answer_text.classList.add('answerbox_valid');
+}
+  else{
+  checked_list.classList.add("answer_invalid");
+  answer_list.classList.add("answer_valid");
+  answer_text.innerHTML = '不正解';
+  answer_text.classList.add('answerbox_invalid');
+}
+
+//非表示解除
+document.getElementById(`answerbox_${answer}`).style.display='block';
+
+    // クリック無効化
+    const parent =checked_list.parentNode;
+    parent.style.pointerEvents = 'none';
+    console.log(parent)
+}
+
 quiz_lists.forEach(function(quiz_list, index){
   // console.log(quiz_list)
   const answer = quiz_list[0];
@@ -24,6 +51,8 @@ quiz_lists.forEach(function(quiz_list, index){
     quiz_list[r] = tmp;
 }
 
+//indexをつける
+
   // console.log(answer);
   // createquestion(answer);
   const contents = `
@@ -31,41 +60,15 @@ quiz_lists.forEach(function(quiz_list, index){
     <h1>${index + 1}. この地名はなんて読む？</h1>
     <img src="./img/${index + 1}.png" alt="" />
     <ul>
-      <li>${quiz_list[0]}</li>
-      <li>${quiz_list[1]}</li>
-      <li>${quiz_list[2]}</li>
-    </ul>
-    <div>
-      <p></p>
+      <li id="quiz_${quiz_list[0]}" onclick="check('${quiz_list[0]}', '${answer}')">${quiz_list[0]}</li>
+      <li id="quiz_${quiz_list[1]}" onclick="check('${quiz_list[1]}', '${answer}')">${quiz_list[1]}</li>
+      <li id="quiz_${quiz_list[2]}" onclick="check('${quiz_list[2]}', '${answer}')">${quiz_list[2]}</li>
+      <li class="answerbox" id="answerbox_${answer}">
+      <span id="quiz_answer_${answer}"></span>
       <p>正解は${answer}です！</p>
-    </div>
+      </li>
+    </ul>
   </div>`
 
   document.getElementById('main').insertAdjacentHTML('beforeend', contents);
 })
-
-  // console.log(quiz_lists);
-  // function createquestion(answer){
-
-  // quiz_lists.forEach(function(quiz_list, index){
-  //   const contents = `
-  // <div class="quiz">
-  //   <h1>${index + 1}. この地名はなんて読む？</h1>
-  //   <img src="./img/${index + 1}.png" alt="" />
-  //   <ul>
-  //     <li>${quiz_list[0]}</li>
-  //     <li>${quiz_list[1]}</li>
-  //     <li>${quiz_list[2]}</li>
-  //   </ul>
-  //   <div>
-  //     <p></p>
-  //     <p>正解は${answer}です！</p>
-  //   </div>
-  // </div>`
-
-  // document.getElementById('main').insertAdjacentHTML('beforeend', contents);
-  // });
-
-  // }
-
-
